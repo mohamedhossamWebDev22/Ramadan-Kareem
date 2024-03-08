fb3706065d5ce4a663ddb6af3538236a25d6e2db
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2024-03-08 20:57:05 +0200

    Add Links

--- src/eng.jsx
+++ src/eng.jsx
@@ -15,13 +15,13 @@ function Eng() {
           <a href="" target="_blank">
             Home
           </a>
-          <a href="" target="_blank">
+          <a href="https://www.equran.org/index.html" target="_blank">
             Quran
           </a>
-          <a href="" target="_blank">
+          <a href="https://mawdoo3.com/%D9%81%D8%B6%D9%84_%D8%B5%D9%8A%D8%A7%D9%85_%D8%B4%D9%87%D8%B1_%D8%B1%D9%85%D8%B6%D8%A7%D9%86" target="_blank">
             Fasting
           </a>
-          <a href="" target="_blank">
+          <a href="https://ar.islamway.net/book/27582/%D8%B5%D8%AD%D9%8A%D8%AD-%D8%A7%D9%84%D8%A8%D8%AE%D8%A7%D8%B1%D9%8A-%D8%B7-%D8%AF%D8%A7%D8%B1-%D8%A7%D8%A8%D9%86-%D9%83%D8%AB%D9%8A%D8%B1" target="_blank">
             Hadith
           </a>
           <NavLink to="/">
